9568aab0a43394beb119f5aba9f2f8392c341b99
Tony <anthonymarques@outlook.com>
2020-07-18 18:48:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { cardArray } from './data/cat-data.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'matchercatcher';
+
+  cardsArray = cardArray;
+
+  
 }
---
+++ src/app/data/cat-data.component.ts
@@ -0,0 +1,41 @@
+export const cardArray = [{
+    name: 'colorCat',
+    img: 'images/colorCat.png'
+},
+{
+    name: 'colorCat',
+    img: 'images/colorCat.png'
+},
+{
+    name: 'blackCat',
+    img: 'images/blackCat.png'
+},
+{
+    name: 'blackCat',
+    img: 'images/blackCat.png'
+},
+{
+    name: 'whiteCat',
+    img: 'images/whiteCat.png'
+},
+{
+    name: 'whiteCat',
+    img: 'images/whiteCat.png'
+},
+{
+    name: 'brownCat',
+    img: 'images/brownCat.png'
+},
+{
+    name: 'brownCat',
+    img: 'images/brownCat.png'
+},
+{
+    name: 'whiteCatScary',
+    img: 'images/whiteCatScary.png'
+},
+{
+    name: 'whiteCatScary',
+    img: 'images/whiteCatScary.png'
+}
+]
