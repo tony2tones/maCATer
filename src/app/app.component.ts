9ae9370894a25a2097d7346adcb6508a5f82d4ec
Anthony <anthonymarques@outlook.com>
2022-09-27 22:36:39 +0200

    added local storage concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CatServiceService } from './cat-service.service';
 import { cardArray } from './data/cat-data.component';
+import { Cat } from './models/cat.model';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +10,35 @@ import { cardArray } from './data/cat-data.component';
 })
 export class AppComponent implements OnInit{
   title = 'matchercatcher';
-
+  localCatPayLoad:Cat[] = [];
+  catPayload:Cat[] = [];
   cardsArray:any;
 
   constructor(private catService: CatServiceService) {}
 
   ngOnInit() {
-    this.catService.getCats().subscribe((data) => {
-      console.log(data);
-    })
-    this.cardsArray = cardArray.sort(() => 0.5 - Math.random())
-    console.log('what is dees', this.cardsArray);
-
+    if(!this.localCatPayLoad && !this.localCatPayLoad.length) {
+      console.log('IS THIS BEING CALLED RIGHT NOW???? ', this.localCatPayLoad);
+      this.catCall();
+    }
+    console.log('this has been ran its all good and has passed')
   }
 
   public catShuffle() {
-    this.ngOnInit();
+    this.localCatPayLoad = this.localCatPayLoad.sort(() => 0.5 - Math.random())
+  }
+
+  public catCall() {
+    console.log('THIS HAS BEEN CALLED');
+    this.catService.getCats().subscribe((data:Cat[]) => {
+      console.log(data);
+      this.catPayload = data;
+      this.catPayload = this.catPayload.sort(() => 0.5 - Math.random())
+      window.localStorage.setItem('catPayload', JSON.stringify(this.catPayload));
+      let localstore = localStorage.getItem('catPayload');
+      const obj = JSON.parse(localstore);
+      console.log('IS IT IN HERE? ',obj);
+      this.localCatPayLoad = obj;
+    })
   }
 }
